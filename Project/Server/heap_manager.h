932114fba14acdd3b38f6302bbeaddad7c85fe74
dusan003 <dusan003gordanic@gmail.com>
2024-11-20 18:37:17 +0100

    adding logic of allocation memory on Server
    New logic for allocate memory.

--- Project/Server/heap_manager.h
+++ Project/Server/heap_manager.h
@@ -1,17 +1,17 @@
 #ifndef HEAP_MANAGER_H
 #define HEAP_MANAGER_H
 
-#include <stddef.h> /
+#include <stddef.h> 
 
 // Struktura koja predstavlja memorijski blok
 typedef struct Block {
-    size_t size;          // Velièina bloka
+    size_t size;          // Velicina bloka
     int is_allocated;     // Status (1 - alociran, 0 - slobodan)
 } Block;
 
-// Struktura koja predstavlja hashmapu za èuvanje blokova
+// Struktura koja predstavlja hashmapu za cuvanje blokova
 typedef struct HashMap {
-    Block* blocks[1000];  // Prepoznavanje blokova sa kljuèem
+    Block* blocks[1000];  // Prepoznavanje blokova sa kljucem
 } HashMap;
 
 // Funkcija za kreiranje hash mape
@@ -26,7 +26,8 @@ void add_block(HashMap* hashmap, int key, Block* block);
 // Funkcija za alokaciju memorije
 Block* allocate_memory(HashMap* hashmap, size_t size);
 
-// Funkcija za oslobaðanje memorije
+// Funkcija za oslobadjanje memorije
 void free_memory(HashMap* hashmap, int key);
 
 #endif // HEAP_MANAGER_H
+
